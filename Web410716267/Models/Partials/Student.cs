c5148998b712c72505487eac3ef34efc033c30cd
邱致豪 <s1071626@gm.pu.edu.tw>
2021-06-17 20:47:48 +0800

    Partials Students

---
+++ Web410716267/Models/Partials/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web410716267.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+
+    public class StudentMetadata
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="請輸入帳號")]
+        [Display(Name = "姓名")]
+        [StringLength(5,MinimumLength =2, ErrorMessage ="請輸入2~5個字")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "請輸入學號")]
+        [Display(Name = "學號")]
+        [StringLength(10, MinimumLength = 5,ErrorMessage = "請輸入5~10個字")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "請輸入Email")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage ="請輸入正確的格式")]
+        public string Email { get; set; }
+
+    }
+}
--- Web410716267/Models/Student.cs
+++ Web410716267/Models/Student.cs
@@ -15,8 +15,12 @@ namespace Web410716267.Models
     public partial class Student
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Number { get; set; }
+
         public string Email { get; set; }
+
     }
 }
